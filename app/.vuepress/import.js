fc870b3c096540f0230c6e4aa37716d94bf06260
Taro Kashiwagi <mad.yc.oc.lp@gmail.com>
2019-08-01 20:27:08 +0900

    画像パス保存部分実装

--- app/.vuepress/import.js
+++ app/.vuepress/import.js
@@ -3,8 +3,27 @@ const fs = require('fs');
 
 axios.get('http://localhost:1337/tests').then(response => {
   console.log(response.data);
-  fs.writeFile('tests.md', response.data[0].strapi, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+  for(let i=0; i<response.data.length; i++){
+    let data = response.data[i];
+
+    let strapi = data.strapi;
+
+    fs.writeFile(`tests${data.id}.md`, strapi, (err) => {
+      let result = strapi.match(/\!\[text\]\(.*\.(png|jpg|jpeg)\)/g);
+      if(result != null) {
+        console.log("success");
+        let res = result[0];
+        let imgpath = res.replace(/\!\[text\]\((.*\.(png|jpg|jpeg))\)/, '$1');
+        console.log(imgpath);
+      } else {
+        console.log("error");
+      }
+
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+
+
+  }
 });
+
---
+++ tests1.md
@@ -0,0 +1,4 @@
+# Title
+## title
+
+![text](/uploads/f95da0f708f846dbb917a715280b5502.png)
