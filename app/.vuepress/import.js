ca9a42400a481ed8786d4925f803c8bcb46a6046
Taro Kashiwagi <mad.yc.oc.lp@gmail.com>
2019-08-02 15:58:18 +0900

    画像を保存させる

--- app/.vuepress/import.js
+++ app/.vuepress/import.js
@@ -5,16 +5,26 @@ axios.get('http://localhost:1337/tests').then(response => {
   console.log(response.data);
   for(let i=0; i<response.data.length; i++){
     let data = response.data[i];
-
     let strapi = data.strapi;
 
+    //strapi以下のtextをmdfileに書き込む
     fs.writeFile(`tests${data.id}.md`, strapi, (err) => {
+      //画像パスを正規表現で検索
       let result = strapi.match(/\!\[text\]\(.*\.(png|jpg|jpeg)\)/g);
       if(result != null) {
-        console.log("success");
         let res = result[0];
         let imgpath = res.replace(/\!\[text\]\((.*\.(png|jpg|jpeg))\)/, '$1');
-        console.log(imgpath);
+        let imgurl = `http://localhost:1337`+imgpath;
+
+        //画像保存
+        axios({
+          method: "get",
+          url: imgurl,
+          responseType: "stream"
+        }).then(function(response) {
+          response.data.pipe(fs.createWriteStream(`./sample${data.id}.png`));
+        });
+        console.log("success");
       } else {
         console.log("error");
       }
@@ -22,8 +32,6 @@ axios.get('http://localhost:1337/tests').then(response => {
       if (err) throw err;
       console.log('The file has been saved!');
     });
-
-
   }
 });
 
